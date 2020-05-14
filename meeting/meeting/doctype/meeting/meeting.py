bd07c62d2843a5a9761e6d3e1b08ffab6a0d7da6
Joseph Marie Alba <josephalbaph@gmail.com>
2020-05-14 03:52:09 +0000

    Second Commit

--- meeting/meeting/doctype/meeting/meeting.py
+++ meeting/meeting/doctype/meeting/meeting.py
@@ -6,9 +6,18 @@ from __future__ import unicode_literals
 import frappe
 from frappe import _
 from frappe.model.document import Document
+from frappe.website.website_generator import WebsiteGenerator
 
-class Meeting(Document):
+class Meeting(WebsiteGenerator):
+	website = frappe._dict(
+		template = "templates/generators/meeting.html"
+	)
+
 	def validate(self):
+		self.page_name = self.name.lower()
+		self.validate_attendees()
+
+	def validate_attendees(self):
 		"""Set missing names"""
 		found = []
 		for attendee in self.attendees:
@@ -17,6 +26,44 @@ class Meeting(Document):
 			if attendee.attendee in found:
 				frappe.throw(_("Attendee {0} entered twice").format(attendee.attendee))
 			found.append(attendee.attendee)
+
+	def sync_todos(self):
+		"""Sync ToDos for assignments"""
+		todos_added = [todo.name for todo in
+                 frappe.get_all("ToDo",
+                                filters={
+                                    "reference_type": self.doctype,
+                                    "reference_name": self.name,
+                               					"assigned_by": ""
+                                })
+                 ]
+
+		for minute in self.minutes:
+			if minute.assigned_to and minute.status == "Open":
+				if not minute.todo:
+					todo = frappe.get_doc({
+						"doctype": "ToDo",
+						"description": minute.description,
+						"reference_type": self.doctype,
+						"reference_name": self.name,
+						"owner": minute.assigned_to
+					})
+					todo.insert()
+
+					minute.db_set("todo", todo.name, update_modified=False)
+
+				else:
+					todos_added.remove(minute.todo)
+
+			else:
+				minute.db_set("todo", None, update_modified=False)
+
+		for todo in todos_added:
+			# remove closed or old todos
+			todo = frappe.get_doc("ToDo", todo)
+			todo.flags.from_meeting = True
+			todo.delete()
+
 
 @frappe.whitelist()					
 def get_full_name(attendee):
